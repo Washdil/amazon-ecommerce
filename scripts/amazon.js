a8e90a54d56f112f39eafc0b6c310fa6beab8414
washdil <washdildm484@gmail.com>
2026-01-18 01:07:52 +0500

    make quantity dropdown interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -27,7 +27,7 @@ products.forEach((product) => {
           </div>
 
           <div class="product-quantity-container">
-            <select>
+            <select class="js-quantity-selector-${product.id}">
               <option selected value="1">1</option>
               <option value="2">2</option>
               <option value="3">3</option>
@@ -68,12 +68,15 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
         }
       })
 
+      const quantitySelector = document.querySelector(`.js-quantity-selector-${button.dataset.productId}`);
+      console.log(quantitySelector)
+
       if(matchingItem) {
-        matchingItem.quantity += 1;
+        matchingItem.quantity += Number(quantitySelector.value);
       } else {
         cart.push({
         productId: productId,
-        quantity: 1
+        quantity: Number(quantitySelector.value)
       });
       }
       
@@ -82,6 +85,10 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
         cartQuantity += item.quantity;
       })
       document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+
+      console.log(cart)
+
+      
   })
 } )
 
