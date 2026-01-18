591e507e08bd93b42bdb5fa5fd41f4bb33fea93e
washdil <washdildm484@gmail.com>
2026-01-18 18:23:56 +0500

    moved addToCart cart.js

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,24 @@
 export const cart = [];
+
+export function addToCart(productId) {
+  let matchingItem;
+    cart.forEach((cartItem) => {
+      if (productId === cartItem.productId) {
+        matchingItem = cartItem;
+      }
+    });
+
+    const quantitySelector = document.querySelector(
+      `.js-quantity-selector-${productId}`,
+    );
+
+    if (matchingItem) {
+      matchingItem.quantity += Number(quantitySelector.value);
+    } else {
+      cart.push({
+        productId: productId,
+        quantity: Number(quantitySelector.value),
+      });
+    }
+}
+
