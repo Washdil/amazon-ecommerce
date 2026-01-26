8473181a6f943f653681a75907bc2993dfa86ff4
washdil <washdildm484@gmail.com>
2026-01-27 00:07:59 +0500

    add jasmine test framework

--- test/moneyTest.js
+++ test/moneyTest.js
@@ -1,10 +1,10 @@
-import {formatCurrency} from "../scripts/utils/money.js";
+import { formatCurrency } from "../scripts/utils/money.js";
 
 console.log("Test Suite: formatCurrency");
 
 console.log("converts cents into dollars");
 
-if (formatCurrency(2095) === "20.95")  {
+if (formatCurrency(2095) === "20.95") {
   console.log("Passed");
 } else {
   console.log("Failed");
@@ -12,7 +12,7 @@ if (formatCurrency(2095) === "20.95")  {
 
 console.log("Works with zero");
 
-if(formatCurrency(0) === '0.00') {
+if (formatCurrency(0) === "0.00") {
   console.log("Passed");
 } else {
   console.log("Failed");
@@ -20,14 +20,14 @@ if(formatCurrency(0) === '0.00') {
 
 console.log("for round up cases");
 
-if(formatCurrency(2000.5) === '20.01') {
-  console.log("Passed"); 
+if (formatCurrency(2000.5) === "20.01") {
+  console.log("Passed");
 } else {
   console.log("Fail");
 }
 
 console.log("round off for less than 0.5");
-if(formatCurrency(2000.4) === '20.00')  {
+if (formatCurrency(2000.4) === "20.00") {
   console.log("Passed");
 } else {
   console.log("Failed");
