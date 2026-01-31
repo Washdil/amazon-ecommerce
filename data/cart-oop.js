cd0ce9a3e1f27b74140cc8122f24d8101d053ee6
washdil <washdildm484@gmail.com>
2026-01-31 20:58:42 +0500

    added inheritance in the code

--- data/cart-oop.js
+++ data/cart-oop.js
@@ -1,28 +1,28 @@
 function Cart(localStorageKey) {
   const cart = {
     cartItems: undefined,
-    loadFromStorage: function () {
+
+    loadFromStorage() {
       this.cartItems = JSON.parse(localStorage.getItem(localStorageKey));
 
       if (!this.cartItems) {
-        this.cartItems = [
-          {
-            productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-            quantity: 2,
-            deliveryOptionId: "1",
-          },
-          {
-            productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-            quantity: 1,
-            deliveryOptionId: "2",
-          },
-        ];
+        this.cartItems = [{
+          productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+          quantity: 2,
+          deliveryOptionId: '1'
+        }, {
+          productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+          quantity: 1,
+          deliveryOptionId: '2'
+        }];
       }
     },
-    saveToStorage: function () {
+
+    saveToStorage() {
       localStorage.setItem(localStorageKey, JSON.stringify(this.cartItems));
     },
-    addToCart: function (productId) {
+
+    addToCart(productId) {
       let matchingItem;
 
       this.cartItems.forEach((cartItem) => {
@@ -37,12 +37,13 @@ function Cart(localStorageKey) {
         this.cartItems.push({
           productId: productId,
           quantity: 1,
-          deliveryOptionId: "1",
+          deliveryOptionId: '1'
         });
       }
 
       this.saveToStorage();
     },
+
     removeFromCart(productId) {
       const newCart = [];
 
@@ -56,10 +57,11 @@ function Cart(localStorageKey) {
 
       this.saveToStorage();
     },
+
     updateDeliveryOption(productId, deliveryOptionId) {
       let matchingItem;
 
-      this.cartItemsforEach((cartItem) => {
+      this.cartItems.forEach((cartItem) => {
         if (productId === cartItem.productId) {
           matchingItem = cartItem;
         }
@@ -68,16 +70,17 @@ function Cart(localStorageKey) {
       matchingItem.deliveryOptionId = deliveryOptionId;
 
       this.saveToStorage();
-    },
+    }
   };
 
   return cart;
 }
 
 const cart = Cart('cart-oop');
+const businessCart = Cart('cart-business');
+
 cart.loadFromStorage();
 
-const businessCart = Cart('cart-business');
 businessCart.loadFromStorage();
 
 console.log(cart);
